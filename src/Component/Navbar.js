9e9fdc0488c1b9c25e4a4f0d987e3feaedab9d66
Pratham <prathamlashkari841@gmail.com>
2023-10-09 23:07:44 +0530

    first-commit

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
-import { Link, useLocation ,useNavigate} from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import logo from '../resources/logo.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function Navbar(props) {
   let navigate = useNavigate();
-  const handleclick=()=>{
+  const handleclick = () => {
     localStorage.removeItem('token');
     navigate('/login');
   }
@@ -24,16 +24,16 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-4">
               <li className="nav-item">
-                <Link className={`nav-link ${location.pathname === '/' ? "active" : ""}`} style={{ fontSize: "1.4em", color: location.pathname === '/' ? 'yellow' : '' }} aria-current="page" to="/">Home</Link>
+                <Link className={`nav-link ${location.pathname === '/' ? "active" : ""}`} style={{ fontSize: "1.4em", color: location.pathname === '/' ? 'yellow' : '', marginRight: '2em' }} aria-current="page" to="/">Home</Link>
               </li>
-              <li className="nav-item mx-5">
+              <li className="nav-item ">
                 <Link className={`nav-link ${location.pathname === '/about' ? "active" : ""}`} style={{ fontSize: "1.4em", color: location.pathname === '/about' ? 'yellow' : '' }} to="about">About</Link>
               </li>
             </ul>
-            {!localStorage.getItem('token')?<form >
-            <Link className="btn btn-outline-info mx-1 btn-sm" to="/login" role="button" aria-disabled="true">Login</Link>
-            <Link className="btn btn-outline-info  mx-3 btn-sm" to="/signup" role="button" aria-disabled="true">Sign Up</Link>
-            </form> :<button className='btn btn-outline-info  mx-3 btn-sm' role='button' onClick={handleclick}>Logout</button> }
+            {!localStorage.getItem('token') ? <form >
+              <Link className="btn btn-outline-info mx-1 btn-sm"  to="/login" role="button" aria-disabled="true">Login</Link>
+              <Link className="btn btn-outline-info  mx-3 btn-sm" to="/signup" role="button" aria-disabled="true">Sign Up</Link>
+            </form> : <button className='btn btn-outline-info  mx-3 btn-sm'  aria-disabled="true" onClick={handleclick}>Logout</button>}
           </div>
         </div>
       </nav>
